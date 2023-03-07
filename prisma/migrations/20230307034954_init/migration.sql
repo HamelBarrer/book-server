00b111120cb14c7c08a8c91bab646c8ca2a75a8f
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-03-06 22:51:31 -0500

    create model and table

---
+++ prisma/migrations/20230307034954_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatar" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "CategoryBook" (
+    "categoryBookId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Book" (
+    "bookId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "categoryBookId" INTEGER NOT NULL,
+    "summary" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("userId") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Book_categoryBookId_fkey" FOREIGN KEY ("categoryBookId") REFERENCES "CategoryBook" ("categoryBookId") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Review" (
+    "reviewId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userUserId" INTEGER NOT NULL,
+    "bookBookId" INTEGER NOT NULL,
+    "qualification" INTEGER NOT NULL,
+    "comment" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Review_userUserId_fkey" FOREIGN KEY ("userUserId") REFERENCES "User" ("userId") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Review_bookBookId_fkey" FOREIGN KEY ("bookBookId") REFERENCES "Book" ("bookId") ON DELETE RESTRICT ON UPDATE CASCADE
+);
